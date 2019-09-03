ceeb0a55f2cda56e40accceded4bc288341ec141
Alex <agordon1123@gmail.com>
2019-09-03 15:28:56 -0400

    set up get requests

--- server.js
+++ server.js
@@ -13,4 +13,37 @@ server.get('/api/posts', (req, res) => {
         })
 });
 
+// Unsure why I'm getting an empty array back on postman when the post with the specific ID is not found
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+    }
+);
+
+// Same comment as above
+server.get('/api/posts/:id/comments', (req, res) => {
+    const id = req.params.id;
+    db.findCommentById(id)
+        .then(coms => {
+            if (coms) {
+                res.status(200).json(coms);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The comments information could not be retrieved." });
+        })
+});
+
 module.exports = server;
