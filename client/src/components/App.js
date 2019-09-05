491fc2321f85e0c7ebcfbb2e146cd8c1efa632dc
Alex <agordon1123@gmail.com>
2019-09-04 22:22:29 -0400

    PUT endpoints working properly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import Axios from 'axios';
+import Post from './Post';
+import { Route, Link } from 'react-router-dom';
 
-const getPosts = () => {
-  Axios.get('http://localhost:8000/api/posts')
-    .then(res => {
-      console.log('res: ', res)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-}
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [updating, setUpdating] = useState(false);
+  console.log(posts);
+
+  const getPosts = () => {
+    Axios.get('http://localhost:8000/api/posts')
+      .then(res => {
+        console.log('res: ', res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
+  
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello from App</h1>
-      <button onClick={getPosts}>GET</button>
+      <h2>Hello Alex</h2>
+      <p>Welcome to MyPlace</p>
+
+      {posts.length && posts.map(post => <Post post={post} />)}
+
+      {/* <Route path='' render={props => <Post {...props} posts={posts} />} /> */}
     </div>
   );
 }
