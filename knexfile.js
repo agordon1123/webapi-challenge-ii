1030bd400c2849823320658fd8886a9d6a9e4135
Alex <agordon1123@gmail.com>
2019-09-06 09:48:38 -0400

    corrected ENV file

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,5 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './data/lambda.db3' },
-    useNullAsDefault: true,
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './data/seeds' },
   },
   production: {
     client: 'sqlite3',
